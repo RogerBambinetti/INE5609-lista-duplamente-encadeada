477b0179b19b787d6d35e59755fb4b85fb536b8e
roger <roger.bambinetti@ozmap.com.br>
2021-12-05 16:06:11 -0300

    adding methods

---
+++ list.py
@@ -0,0 +1,67 @@
+class List:
+
+    def __init__(self, value: int, next):
+        self.__value = value
+        self.__next = next
+    
+    def acessarAtual():
+        return 1
+    
+    def inserirAntesDoAtual(novo):
+        return 1
+
+    def inserirApósAtual(novo):
+        return 1
+
+    def inserirNoFim(novo):
+        return 1
+
+    def inserirNaFrente(novo):
+        return 1
+
+    def inserirNaPosicao ( k, novo ):
+        return 1
+    
+    def excluirAtual ():
+        return 1
+    
+    def excluirPrim ():
+        return 1
+
+    def excluirUlt():
+        return 1
+
+    def excluirUlt():
+        return 1
+
+    def excluirElem(chave):
+        return 1
+
+    def excluirDaPos (k):
+        return 1
+
+    def buscar (chave):
+        return 1
+
+    def avançarKPosições( K ):
+        return 1
+
+    def retrocederKPosições ( K ):
+        return 1
+
+    def irParaPrimeiro():
+        return 1
+
+    def irParaUltimo():
+        return 1
+
+    def vazia():
+        return 1
+
+    def cheia():
+        return 1
+
+    def posiçãoDe(chave):
+        return 1
+
+    
