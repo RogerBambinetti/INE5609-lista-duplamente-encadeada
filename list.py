2462a8d34ce035cff1c7a4a6033479997f7d08bf
Rafa <rafaelatillmann.rt@gmail.com>
2021-12-05 19:31:02 -0300

    adicionando operaçoes padrao

--- list.py
+++ list.py
@@ -1,49 +1,211 @@
+from element import Element
+
+
 class List:
 
     def __init__(self, size: int):
-        self.__cursor = 0
-        self.__start = 0
-        self.__ending = 0
+        self.__cursor = None
+        self.__start = None
+        self.__ending = None
         self.__size = size
         self.__count = 0
 
-    def acessarAtual():
-        return 1
+    @property
+    def start(self):
+        return self.__start
 
-    def inserirAntesDoAtual(novo):
-        return 1
+    @start.setter
+    def start(self, start):
+        self.__start = start
 
-    def inserirApósAtual(novo):
-        return 1
+    @property
+    def ending(self):
+        return self.__ending
 
-    def inserirNoFim(novo):
-        return 1
+    @ending.setter
+    def ending(self, ending):
+        self.__ending = ending
 
-    def inserirNaFrente(novo):
-        return 1
+    # -----------
 
-    def inserirNaPosicao(k, novo):
-        return 1
+    def acessarAtual(self):
+        return self.__cursor
 
-    def excluirAtual():
-        return 1
+    def inserirAntesDoAtual(self, value):
+        if not self.cheia():
 
-    def excluirPrim():
-        return 1
+            previous = self.__cursor.previous
+            cursor = self.__cursor
 
-    def excluirUlt():
-        return 1
+            element = Element(previous, value, cursor)
 
-    def excluirUlt():
-        return 1
+            previous.next = element
+            cursor.previous = element
 
-    def excluirElem(chave):
-        return 1
+            self.__count += 1
+        else:
+            raise Exception
 
-    def excluirDaPos(k):
-        return 1
+    def inserirApósAtual(self, value):
+        if not self.cheia():
 
-    def buscar(chave):
+            next = self.__cursor.next
+            cursor = self.__cursor
+
+            element = Element(cursor, value, next)
+
+            next.previous = element
+            cursor.next = element
+
+            self.__count += 1
+        else:
+            raise Exception
+
+    def inserirNoFim(self, value):
+        if not self.cheia():
+
+            self.irParaUltimo()
+            cursor = self.__cursor
+
+            element = Element(cursor, value, None)
+
+            cursor.next = element
+            self.__ending = element
+
+            self.__count += 1
+        else:
+            raise Exception
+
+    def inserirNaFrente(self, value):
+        if not self.cheia():
+
+            self.irParaPrimeiro()
+            cursor = self.__cursor
+
+            element = Element(None, value, cursor)
+
+            cursor.previous = element
+            self.__start = element
+
+            self.__count += 1
+        else:
+            raise Exception
+
+    def inserirNaPosicao(self, k, value):
+        if not self.cheia():
+
+            p = self.posiçãoDe(self.__cursor)
+
+            if k > p:
+                n = k - p
+                self.avançarKPosições(n)
+            elif k < p:
+                n = p - k
+                self.retrocederKPosições(n)
+
+            previous = self.__cursor.previous
+            cursor = self.__cursor
+
+            element = Element(previous, value, cursor)
+
+            previous.next = element
+            cursor.previous = element
+
+            self.__count += 1
+
+        else:
+            raise Exception
+
+    def excluirAtual(self):
+        if not self.vazia():
+
+            previous = self.__cursor.previous
+            next = self.__cursor.next
+
+            previous.next = next
+            next.previous = previous
+            self.__cursor = previous
+
+            self.__count -= 1
+
+        else:
+            raise Exception
+
+    def excluirPrim(self):
+        if not self.vazia():
+
+            self.irParaPrimeiro()
+
+            next = self.__cursor.next
+
+            next.previous = None
+
+            self.__cursor = next
+            self.__start = next
+
+            self.__count -= 1
+
+        else:
+            raise Exception
+
+    def excluirUlt(self):
+        if not self.vazia():
+
+            self.irParaUltimo()
+
+            previous = self.__cursor.previous
+
+            previous.next = None
+
+            self.__cursor = previous
+            self.__ending = previous
+
+        else:
+            raise Exception
+
+    def excluirElem(self, key):
+        if not self.vazia():
+
+            if self.buscar(key):
+
+                element = key
+
+                previous = element.previous
+                next = element.next
+
+                previous.next = next
+                next.previous = previous
+
+                self.__count -= 1
+
+        else:
+            raise Exception
+
+    def excluirDaPos(self, k):
+        if not self.vazia():
+
+            p = self.posiçãoDe(self.__cursor)
+
+            if k > p:
+                n = k - p
+                self.avançarKPosições(n)
+            elif k < p:
+                n = p - k
+                self.retrocederKPosições(n)
+
+            previous = self.__cursor.previous
+            next = self.__cursor.next
+
+            previous.next = next
+            next.previous = previous
+            self.__cursor = previous
+
+            self.__count -= 1
+
+        else:
+            raise Exception
+
+    def buscar(key):
         return 1
 
     def avançarKPosições(K):
@@ -70,5 +232,5 @@ class List:
         else:
             return False
 
-    def posiçãoDe(chave):
+    def posiçãoDe(key):
         return 1
